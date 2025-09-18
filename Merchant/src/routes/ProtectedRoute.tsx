f0654707d5857ad5f581047f08cb2ddee2aa038d
Tein Fai <teinfai@gmail.com>
2025-09-18 22:32:26 +0800

    debug

--- Merchant/src/routes/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "../app/store";
-
-export default function ProtectedRoute() {
-  const accessToken = useSelector((s: RootState) => s.auth.accessToken);
-  return accessToken ? <Outlet /> : <Navigate to="/login" replace />;
-}
